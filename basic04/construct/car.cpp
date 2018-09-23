2dd155d4702922fc8a38b0f469140793a3d9323b
solareenlo <solareenlo@gmail.com>
2018-09-23 14:02:48 +0900

    コンストラクタとデストラクタの練習

---
+++ basic04/construct/car.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include "car.h"
+
+using namespace std;
+
+CCar::CCar() : m_fuel(0), m_migration(0) {
+    cout << "CCarオブジェクト生成" << endl;
+}
+
+CCar::~CCar() {
+    cout << "CCarオブジェクト破棄" << endl;
+}
+
+void CCar::move() {
+    if(m_fuel >= 0) {
+        m_migration++;
+        m_fuel--;
+    }
+    cout << "移動距離：" << m_migration << endl;
+    cout << "残りの燃料：" << m_fuel << endl;
+}
+
+void CCar::supply(int fuel) {
+    if(fuel > 0) {
+        m_fuel += fuel;
+    }
+    cout << "燃料：" << m_fuel << endl;
+}
---
+++ basic04/construct/main.cpp
@@ -0,0 +1,10 @@
+#include "car.h"
+
+int main(void) {
+    CCar c;
+    c.supply(20);
+    c.move();
+    c.move();
+    c.move();
+    return 0;
+}
