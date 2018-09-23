dd9a7c5fe622c83a472b3dfed3f57c6188b179b0
solareenlo <solareenlo@gmail.com>
2018-09-23 13:29:40 +0900

    2つの整数の和と差を出力

---
+++ basic03/prob/prob3_3/calculation.h
@@ -0,0 +1,21 @@
+#ifndef _CALCULATION_H_
+#define _CALCULATION_H_
+
+#include <iostream>
+
+using namespace std;
+
+class Calculation {
+    private:
+        int m_number1;
+        int m_number2;
+    public:
+        void setNumber1(int n);
+        void setNumber2(int n);
+        int getNumber1();
+        int getNumber2();
+        int add();
+        int sub();
+};
+
+#endif
