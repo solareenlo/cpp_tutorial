467b33dc33ff517b096fee95ce814a1cabf9217c
solareenlo <solareenlo@gmail.com>
2018-09-23 15:50:55 +0900

    newとdeleteを用いたメモリの確保と解放の練習

--- basic04/new_delete_int/main.cpp
+++ basic04/new_delete_int/main.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 int main(void) {
     int *p = 0;
-    p = new int();
+    p = new int(); // int型の領域を動的に確保
     *p = 123456;
     cout << *p << endl;
-    delete p;
+    delete p; // 動的に確保した領域を解放
     return 0;
 }
