2e319a3b2edd7cd44bc88647f9fc4acf283fa5f7
solareenlo <solareenlo@gmail.com>
2018-09-24 15:14:44 +0900

    引数の参照渡しの練習

---
+++ advanced01/call_by_reference/call_by_reference.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+void ref(int&);
+void print(int);
+
+int main(void) {
+    int n = 5;
+    print(n);
+    ref(n);
+    print(n);
+    return 0;
+}
+
+void ref(int& n) {
+    n = 1;
+}
+
+void print(int n) {
+    cout << "n = " << n << endl;
+}
