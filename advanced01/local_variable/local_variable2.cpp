99a40e6030ada35b3421120c9c769dec9f750149
solareenlo <solareenlo@gmail.com>
2018-09-24 15:22:06 +0900

    ローカル変数の定義の練習その2

---
+++ advanced01/local_variable/local_variable2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    cout << "ABCDEF" << endl;
+    for(int i = 0; i < 10; i++) {
+        cout << " : " << i;
+    }
+    cout << " : " << endl;
+    return 0;
+}
