e68552c09c9eb82fb9c0f46306007adc2f10eb0b
solareenlo <solareenlo@gmail.com>
2018-09-27 22:12:30 +0900

    mapを使って、動物の英単語を入力するとそれに対応する日本語が出力される

---
+++ advanced05/prob/prob5_1/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <map>
+
+using namespace std;
+
+int main(void) {
+    int i;
+    string s;
+    string name[] = {"cat", "dog", "bird", "tiger"};
+    map<string, string> animal;
+    animal[name[0]] = "猫";
+    animal[name[1]] = "犬";
+    animal[name[2]] = "鳥";
+    animal[name[3]] = "トラ";
+
+    cout << "英語で動物の名前(cat, dog, bird, tigerのうちいずれか)を入力してください：";
+    cin >> s;
+    cout << endl;
+
+    for(i=0; i<4; i++)
+        if(s == name[i])
+            cout << "「" << animal[name[i]] << "」です" << endl;
+
+    return 0;
+}
