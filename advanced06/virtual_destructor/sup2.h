bc3e1cc88fb3ab71e3e0a1fa8895e06b38b9480d
solareenlo <solareenlo@gmail.com>
2018-09-29 05:07:19 +0900

    仮想デストラクタの注意点の練習

---
+++ advanced06/virtual_destructor/sup2.h
@@ -0,0 +1,12 @@
+#ifndef _SUP2_H_
+#define _SUP2_H_
+
+using namespace std;
+
+class CSup2 {
+    public:
+        CSup2() {cout << "CSup2のコンストラクタ" << endl;}
+        virtual ~CSup2() {cout << "CSup2のデストラクタ" << endl;}
+};
+
+#endif
